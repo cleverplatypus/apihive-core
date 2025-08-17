d8c5b1f313492577689e90b7629be3bc096a3cc3
Nicola Dal Pont <nicola.dal.pont@egrowcery.com>
2025-08-17 19:17:06 +0930

    chore: removed lib from repo

--- lib/constants.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const DEFAULT_JSON_MIME_TYPES: string[];
-export declare const DEFAULT_TEXT_MIME_TYPES: string[];
--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './types.js';
-export * from './HTTPRequestFactory.js';
-export * from './adapter-types.js';
-export * from './HTTPRequest.js';
